c16b58515841d77ae498d011b6dc2e8aaf61679f
Titi-shop <226829233+Titi-shop@users.noreply.github.com>
2026-02-08 16:24:43 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const PI_ONLY = process.env.PI_BROWSER_ONLY === "false";
+const PI_ONLY = process.env.PI_BROWSER_ONLY === "true";
 
 // Heuristic UA check
 function isPiBrowser(req: NextRequest) {
